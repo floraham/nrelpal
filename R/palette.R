80d2bafb5163b1e287d77b9f21110120ca5f0f27
floraham <floraham@outlook.com>
2025-11-01 19:48:09 -0600

    Update palettes and streamline with csu_pal()

--- R/palette.R
+++ R/palette.R
@@ -1,16 +1,40 @@
-#' NREL Core Palette
+#' CSU Primary Palette
 #'
-#' Returns a vector of hex colors used by the lab.
+#' Returns a vector of primary CSU/NREL lab colors.
 #' @export
-nrel_palette <- function() {
-  c("#355834", "#6B8E23", "#C2B280", "#D9BF77", "#EAE0C8")
+csu_primary_palette <- function() {
+  c("#1E4D2B", "#C8C372", "#D9782D", "#FFFFFF", "#59595B")
 }
 
-#' Get n Colors from NREL Palette
+#' CSU Energy Palette
 #'
-#' Interpolates the NREL palette to n colors.
-#' @param n Number of colors to return
+#' Returns a vector of colors used for CSU energy-themed graphics.
 #' @export
-nrel_pal <- function(n) {
-  colorRampPalette(nrel_palette())(n)
+csu_energy_palette <- function() {
+  c("#008FB3", "#105456", "#006144", "#82C503", "#CFFC00", "#FFC038", "#E56A54", "#7E5475")
+}
+
+#' CSU Palettes
+#'
+#' Access and interpolate CSU color palettes.
+#'
+#' @param palette Which palette to use: "primary" or "energy".
+#' @param n Number of colors to return. If `NULL`, returns all colors in the palette.
+#' @return A vector of hex color codes.
+#' @examples
+#' csu_pal("primary")
+#' csu_pal("energy", 10)
+#' @export
+csu_pal <- function(palette = c("primary", "energy"), n = NULL) {
+  palette <- match.arg(palette)
+  
+  colors <- switch(palette,
+                   primary = csu_primary_palette(),
+                   energy = csu_energy_palette())
+  
+  if (is.null(n)) {
+    return(colors)
+  } else {
+    return(colorRampPalette(colors)(n))
+  }
 }
